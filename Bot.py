5397777a651d767de6c50323d4df6617d971e3bb
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-11 12:02:04 +0530

    BrandedXMention

--- Bot.py
+++ Bot.py
@@ -23,7 +23,7 @@ LOGGER = logging.getLogger(__name__)
 
 api_id = int(os.environ.get("APP_ID", ""))
 api_hash = os.environ.get("API_HASH", "")
-bot_token = os.environ.get("TOKEN", "")
+bot_token = os.environ.get("BOT_TOKEN", "")
 client = TelegramClient("client", api_id, api_hash).start(bot_token=bot_token)
 spam_chats = []
 
